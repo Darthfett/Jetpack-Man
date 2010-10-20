5930e394af6c39b8cb7887398aa728c37f618770
Darthfett <darthfett@gmail.com>
2010-10-19 18:06:37 -0700

    Fixed a small collision issue

--- run_game.py
+++ run_game.py
@@ -9,4 +9,3 @@ import sys, os
 sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
 
 import Main
-
--- src/Main/Entity.py
+++ src/Main/Entity.py
@@ -5,6 +5,7 @@ It can have velocity, acceleration, and several other properties
 """
 
 from Object import Object
+import math
 
 class Entity(Object):
     Entities = []
@@ -18,14 +19,16 @@ class Entity(Object):
             self.currentAnimation = self.objectType.animations['move']
 
         return self.currentAnimation.frame[0]
-    
+
     def onLand(self):
         """
         Called upon landing on an object
         """
         self.velocity[1] = 0
-        
-    def onObjectCollision(self,object):
+        self.acceleration[1] = 0
+        pass
+
+    def onObjectCollision(self, object):
         """
         Upon collision with an Object, entities are moved towards the side of the bounding rect that they would have hit first.
         """
@@ -37,12 +40,9 @@ class Entity(Object):
             self.position[1] = (object.position[1] + object.objectType.height) if (self.velocity[1] < 0) else (object.position[1] - self.objectType.height)
             if self.velocity[1] > 0:
                 self.onLand()
-                
-            self.acceleration[1] = 0
-            
             self.velocity[1] = 0
             return
-            
+
         if self.velocity[1] > 0:
             vyDiff = object.position[1] - (self.position[1] + self.objectType.height)
         elif self.velocity[1] < 0:
@@ -50,25 +50,25 @@ class Entity(Object):
         else:
             self.position[0] += vxDiff
             return
-        
-        if (abs(vyDiff/self.velocity[1]) > abs(vxDiff / self.velocity[0])):
+
+        if (abs(vyDiff / self.velocity[1]) > abs(vxDiff / self.velocity[0])):
             self.velocity[0] = 0
-            self.position[0] += vxDiff
+            self.position[0] += vxDiff + math.copysign(1, vxDiff)
         else:
-            self.position[1] += vyDiff
-            self.acceleration[1] = 0
+            self.position[1] += vyDiff + math.copysign(1, vyDiff)
+            if self.velocity[1] > 0:
+                self.onLand()
             self.velocity[1] = 0
-            
-            
+
+
 
     def __init__(self, whichType, position = [0, 0], velocity = [0, 0], acceleration = [0, 0], flipped = False):
         """
         Creates a basic Entity of a specific type.
         """
-        Object.__init__(self, whichType, position, flipped=flipped)
+        Object.__init__(self, whichType, position, flipped = flipped)
         Entity.Entities.append(self)
         Object.Objects.pop(-1)
-        
+
         self.velocity = velocity
         self.acceleration = acceleration
-        
